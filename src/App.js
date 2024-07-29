39f81b2478e0b8a4263e6e2a8114b608b6651dac
Ben Ho <bennyhch@gmail.com>
2024-07-29 14:40:07 +0100

    feat: add route and clean up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 import "./App.css";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+import Dingo from "./pages/scholars/Dingo";
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/dingo" element={<Dingo />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
