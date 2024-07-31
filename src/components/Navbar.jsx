396a40a6f6f3f55c37ee8df386d1932bc3f1a5b9
Ben Ho <bennyhch@gmail.com>
2024-07-31 09:48:53 +0100

    fix: refactor navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const aboutMenuItems = [
   { href: "contact-us", component: ContactUs },
 ];
 
-const NavLink = ({ href, children, menuItems }) => {
+const NavLink = ({ href, text, menuItems }) => {
   return (
     <li className="relative group">
       {/* <a href={href} className="hover:underline "> */}
@@ -24,7 +24,7 @@ const NavLink = ({ href, children, menuItems }) => {
         href={href}
         className="hover:underline focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center"
       >
-        {children}
+        {text}
       </a>
       {menuItems && (
         <div className="absolute left-0 mt-2 z-10 overflow-hidden transition-all duration-300 transform scale-y-0 opacity-0 group-hover:scale-y-100 group-hover:opacity-100 origin-top bg-white border rounded-lg shadow-lg">
@@ -50,17 +50,17 @@ const Navbar = () => {
   return (
     <nav className="py-3">
       <ul className="flex justify-center gap-x-10">
-        <NavLink href="/">HOME</NavLink>
-        <NavLink href="/about" menuItems={aboutMenuItems}>
-          ABOUT
-        </NavLink>
-        <NavLink href="/scholarship" menuItems={scholarshipMenuItems}>
-          SCHOLARSHIP
-        </NavLink>
-        <NavLink href="/aid">HUMANITARIAN AID</NavLink>
-        <NavLink href="/donate">DONATE</NavLink>
-        <NavLink href="/art">ART</NavLink>
-        <NavLink href="/faqs">FAQS</NavLink>
+        <NavLink href="/" text="HOME" />
+        <NavLink href="/about" menuItems={aboutMenuItems} text="ABOUT" />
+        <NavLink
+          href="/scholarship"
+          menuItems={scholarshipMenuItems}
+          text="SCHOLARSHIP"
+        />
+        <NavLink href="/aid" text="HUMANITARIAN AID" />
+        <NavLink href="/donate" text="DONATE" />
+        <NavLink href="/art" text="ART" />
+        <NavLink href="/faqs" text="FAQS" />
       </ul>
     </nav>
   );
