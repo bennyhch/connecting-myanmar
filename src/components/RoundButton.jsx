e74d129248069ca74571db2a75237f3d01f37555
Ben Ho <bennyhch@gmail.com>
2024-07-31 17:52:11 +0100

    feat: add footer

---
+++ src/components/RoundButton.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const RoundButton = ({ text, color, link, newTab }) => {
+  return (
+    <button
+      onClick={() =>
+        window.open(
+          link,
+          newTab ? "_blank" : "_self",
+          "rel=noopener noreferrer"
+        )
+      }
+      className={`p-4 px-10 rounded-full ${color} w-60 hover:brightness-75`}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default RoundButton;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "../components/Button";
 import Divider from "../components/Divider";
+import RoundButton from "../components/RoundButton";
 
 const sectionWrapper = "max-w-xl w-full mx-auto my-50";
 
@@ -11,19 +12,19 @@ const Heading = ({ text, textColor }) => {
 const Home = () => {
   return (
     <div>
-      <div className="relative h-70lvh">
-        {/* <div className="bg-[url('/src/assets/home-intro.jpg')] h-full bg-cover blur-sm text-black"> */}
+      <header className="relative h-70lvh">
         <div className="absolute inset-0 bg-[url('/src/assets/home-intro.jpg')] h-full bg-cover bg-center text-black blur-sm"></div>
-        <p className="absolute text-white w-1/4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          WE ENVISION AN <span className="text-customGreen">EMPATHETIC</span>{" "}
-          WORLD WHERE EVERYONE HAS THE ​EQUAL OPPORTUNITY TO ​REALIZE THEIR
-          POTENTIAL AND ASPIRATIONS
+        <p className="text-xl leading-normal font-bold absolute text-white w-1/3 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          WE ENVISION AN{" "}
+          <span className="text-customGreen">EMPATHETIC WORLD</span> WHERE
+          EVERYONE HAS THE ​EQUAL OPPORTUNITY TO ​REALIZE THEIR POTENTIAL AND
+          ASPIRATIONS
         </p>
-      </div>
+      </header>
 
       <main className="flex justify-center items-center flex-col w-full">
         <section className={`${sectionWrapper} mt-14`}>
-          <Heading text="OUR MISSION" textColor="text-customGod" />
+          <Heading text="OUR MISSION" textColor="text-customGold" />
           <p className="mb-3">
             To inspire youth to think critically with an empathetic lens through
             connecting Myanmar and HK youth. To empower youth through the
@@ -31,7 +32,7 @@ const Home = () => {
             towards positive change.
           </p>
           <Button link="/about" text="LEARN MORE" />
-          <Divider marginY={8} />
+          <Divider />
         </section>
 
         <section className={sectionWrapper}>
@@ -42,11 +43,11 @@ const Home = () => {
             community.
           </p>
           <Button link="/patrons" text="LEARN MORE" />
-          <Divider marginY={8} />
+          <Divider />
         </section>
 
         <section className={sectionWrapper}>
-          <Heading text="VOLUNTEER PROGRAMMES" textColor="text-customGod" />
+          <Heading text="VOLUNTEER PROGRAMMES" textColor="text-customGold" />
           <p className="mb-3">
             We provide a platform for knowledge exchange - through supporting
             students to pursue their social calling, we hope to create an
@@ -56,6 +57,32 @@ const Home = () => {
           <Button link="/programmes" text="LEARN MORE" />
         </section>
       </main>
+
+      <footer className="h-70lvh mt-8">
+        <div className="bg-[url('/src/assets/footer.jpg')] h-full bg-cover bg-center relative">
+          <div className="absolute inset-0 bg-black opacity-70"></div>
+          <div className="relative z-10 flex flex-col items-center justify-center h-full text-white text-center space-y-4">
+            <p className="w-1/2">
+              All profit from the artwork sale goes to support our scholarship
+              program, sending ​Myanmar students to Universities. Visit us on
+              Artsy:
+            </p>
+            <div className="mt-8 w-1/2 flex justify-around">
+              <RoundButton
+                link="https://www.artsy.net/partner/connecting-myanmar"
+                text="ARTSY"
+                color="bg-customGold"
+                newTab
+              />
+              <RoundButton
+                link="/donate"
+                text="DONATE NOW"
+                color="bg-customGreen"
+              />
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 };
