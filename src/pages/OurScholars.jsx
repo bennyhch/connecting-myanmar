c8e169b42719e3008d69a18473151db4174ef7fe
Ben Ho <bennyhch@gmail.com>
2024-08-02 13:58:55 +0100

    feat: basic scholars layout

--- src/pages/OurScholars.jsx
+++ src/pages/OurScholars.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import HeaderImageWrapper from "../components/HeaderImage";
+import Button from "../components/Button";
+
+import { SectionHeading } from "../utils/wrappers";
+import { scholars } from "../utils/scholars";
+
+const Gallery = () => {
+  return (
+    <div className="gallery">
+      {scholars.map(({ name, img, quote }, index) => (
+        <div className="card" key={index}>
+          <h3 className="text-customGold font-bold">{name}</h3>
+          <div className="image-container">
+            <img src={img} alt={name} />
+          </div>
+          <div className="text-container">
+            <p>{quote}</p>
+          </div>
+          <Button text={`More about ${name}`} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
 const OurScholars = () => {
-  return <div>OurScholars</div>;
+  return (
+    <div>
+      <HeaderImageWrapper
+        isShading
+        imgUrl="bg-[url('/src/assets/ourScholars.jpg')]"
+      >
+        MEET OUR <span className="text-customGreen">STUDENTS</span>
+      </HeaderImageWrapper>
+
+      <main className="w-full text-justify">
+        <section className="max-w-5xl w-full mx-auto my-50 mt-14">
+          <SectionHeading text="OUR SCHOLARS" textColor="text-customGreen" />
+          <p>
+            We are currently funding <b>fourteen</b> Myanmar students, thirteen
+            of whom study in the universities in Thailand and one goes to a
+            university in Taiwan. Read their stories below.
+          </p>
+        </section>
+        <section className="max-w-5xl w-full mx-auto my-50 ">
+          <Gallery />
+          {/* <div className="gallery">
+            {scholars.map((scholar, index) => (
+              <GalleryCard key={index} {...scholar} />
+            ))}
+          </div> */}
+        </section>
+      </main>
+    </div>
+  );
 };
 
 export default OurScholars;
---
+++ src/utils/scholars.js
@@ -0,0 +1,94 @@
+import aunghtetoo from "../assets/scholars/aunghtetoo.png";
+import blessing from "../assets/scholars/blessing.png";
+import deeku from "../assets/scholars/deeku.png";
+import dingo from "../assets/scholars/dingo.png";
+import htinlinnzaw from "../assets/scholars/htinlinnzaw.png";
+import naykawhtoo from "../assets/scholars/naykawhtoo.png";
+import naymin from "../assets/scholars/naymin.png";
+import naysoe from "../assets/scholars/naysoe.png";
+import nyinyitun from "../assets/scholars/nyinyitun.png";
+import satkyar from "../assets/scholars/satkyar.png";
+import sawsiyar from "../assets/scholars/sawsiyar.png";
+import thanhtikezin from "../assets/scholars/thanhtikezin.png";
+import tony from "../assets/scholars/tony.png";
+
+export const scholars = [
+  {
+    name: "Dingo",
+    quote:
+      '"I wish to set up my own social enterprise one day – using what I have learnt at school and the experience I have gained throughout the years to create a better living place for my community and people in Myanmar!"',
+    img: dingo,
+  },
+  {
+    name: "Satkyar",
+    quote:
+      '"When I was 18, I found my passion in writing and telling people’s stories of injustice. This started my dream to become a journalist."',
+    img: satkyar,
+  },
+  {
+    name: "Blessing",
+    quote:
+      '"When I was 15, my family moved to Mae La refugee camp. After finishing high school at the camp, I worked at Mae Fah Luang Foundation as a nurse aid for five years. ​I wished to study nursing and become a registered nurse in Myanmar one day."',
+    img: blessing,
+  },
+  {
+    name: "Deeku",
+    quote:
+      '"I understand the importance of having equal access to quality education. With the precious opportunity of doing a business degree in overseas university, I wish that I could share my knowledge with Burmese youth. I hope that one day I could transform the landscape of business and education in Myanmar."',
+    img: deeku,
+  },
+  {
+    name: "Nay Kaw Htoo",
+    quote:
+      "​\"One of my greatest achievements in life is working in a reputable NGO where I could create positive social impact. Currently, I am pursuing a Bachelor's degree in Social Sciences. The knowledge I've gained would help me contribute to meaningful community initiatives after I graduate. By empowering myself, I hope to empower others.\"",
+    img: naykawhtoo,
+  },
+  {
+    name: "Tony",
+    quote:
+      '"I hope to improve my English skills and have more confidence in communicating with other people. I wish that I would be capable of working with others and helping my community."',
+    img: tony,
+  },
+  {
+    name: "Nay Min",
+    quote:
+      '"The idea of becoming a civil engineer came to me when I endured the long journeys between my school in Thailand and my hometown in Myanmar. I hope to bridge the gap between  Myanmar and the world."',
+    img: naymin,
+  },
+  {
+    name: "Than Htike Zin",
+    quote:
+      '"I graduated top of my class in secondary school and was grateful to be accepted into the computer science programme of a renowned university. I hope to use technology to improve people\'s lives."',
+    img: thanhtikezin,
+  },
+  {
+    name: "Aung Htet Oo",
+    quote:
+      '"Being born in a village without healthcare facilities was like being born without hope. I wish to qualify as a nurse and to open a clinic in my hometown."',
+    img: aunghtetoo,
+  },
+  {
+    name: "Nyi Nyi Tun",
+    quote:
+      '"My family used to lead a humble life as farmers. But when pests destroyed our land, we migrated to a refugee camp for shelter. I hope to become an entrepreneur to improve the livelihood of my family and many other rural residents."',
+    img: nyinyitun,
+  },
+  {
+    name: "Htin Lin Zaw",
+    quote:
+      '"After interning at a large NGO, I appreciated the importance of business knowledge - it enables one to run organizations or companies effectively in order to create social value. This inspired me to study Business Administration. In the short term, I aspire to work in the same NGO that I interned at; in the long run, I hope to establish my own business."',
+    img: htinlinnzaw,
+  },
+  {
+    name: "Saw Siyar",
+    quote:
+      '"Since I was young, I had my heart set on helping others. After engaging in internships, I\'ve discovered an effective way improving the well-being of rural residents - to start a sustainable business and create job opportunities"',
+    img: sawsiyar,
+  },
+  {
+    name: "Nay Soe",
+    quote:
+      '"Under the encouragement of a teacher, I made it to a Thai University despite my disadvantaged background as a refugee. I aspire to follow his footsteps and become a teacher. I hope to inspire students like my younger self."',
+    img: naysoe,
+  },
+];
