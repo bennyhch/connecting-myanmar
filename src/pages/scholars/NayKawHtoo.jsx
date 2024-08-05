3cae07cf48db045f2b57bfbcbbc27c7629c4611a
Ben Ho <bennyhch@gmail.com>
2024-08-05 12:02:45 +0100

    feat: add naymin

--- src/pages/scholars/NayKawHtoo.jsx
+++ src/pages/scholars/NayKawHtoo.jsx
@@ -36,73 +36,70 @@ const facts = [
   },
 ];
 
-const NayKawHtoo = () => {
-  return (
-    <div>
-      <ScholarHeader
-        imgUrl="bg-[url('/src/assets/scholars/sawnaykawhtoo/sawnaykawhtoo_cover.jpg')]"
-        name="saw nay kaw htoo"
-        program="Social Sciences"
-        university="Chiang Mai University"
+const NayKawHtoo = () => (
+  <div>
+    <ScholarHeader
+      imgUrl="bg-[url('/src/assets/scholars/sawnaykawhtoo/sawnaykawhtoo_cover.jpg')]"
+      name="saw nay kaw htoo"
+      program="Social Sciences"
+      university="Chiang Mai University"
+    />
+    <MainContentWrapper>
+      <ScholarArticleWrapper name="saw nay kaw htoo">
+        <ParagraphWrapper>
+          Hi, I’m Saw Nay Kaw Htoo! After graduating from high school, I did not
+          give too much thought about pursuing a bachelor’s degree – only two
+          people from my village went to university. Instead, I responded to a
+          strong calling I’ve had since I was young, which was to engage in
+          community work.
+        </ParagraphWrapper>
+
+        <ParagraphWrapper>
+          Hence, after high school graduation, I trained and qualified as a
+          Community Health Worker at Mae Tao Clinic at the Thai-Myanmar border,
+          providing medical treatment to villagers and refugees. I am grateful
+          that during my two years there, I was able to care for the ill and
+          weak. However, I also noticed other problems that our patients were
+          mired in, including poverty and threats from armed conflicts. I began
+          to develop an interest in advancing broader social justice.
+        </ParagraphWrapper>
+
+        <ParagraphWrapper>
+          In order to learn more about law, social sciences and political
+          sciences, I completed a diploma at the Peace Law Academy. Afterwards,
+          I joined a United Nations-funded NGO called the “Adventist Development
+          and Relief Agency (ADRA) Myanmar”. As the Community Mobilizer, I
+          liaised with village leaders in executing community-wide projects like
+          a community-based health insurance scheme. Contributing to such
+          projects gave me an unprecedented sense of fulfilment. I wished to
+          follow the footsteps of my colleagues who had bachelor’s degrees, who
+          could play even greater roles such as planning and design. Hence, I
+          completed the General Education Diploma and was very happy to be
+          accepted by Chiang Mai University, where I am currently studying
+          Social Sciences.
+        </ParagraphWrapper>
+
+        <ParagraphWrapper>
+          To me, education is about empowerment – I hope to empower myself and
+          thus to empower others. Myanmar has been through a lot in recent
+          years, so community work will be ever-more important. With my passion,
+          knowledge and experience, I hope to engage in meaningful projects and
+          to contribute to society.
+        </ParagraphWrapper>
+      </ScholarArticleWrapper>
+
+      <ScholarProfile
+        profilePicture={nayKawHtoo}
+        name="Saw Nay Kaw Htoo"
+        ethnicity="Karen"
+        placeOfBirth="Karen State, Myanmar"
+        age="29"
+        programs={programs}
+        goals={goals}
+        facts={facts}
       />
-      <MainContentWrapper>
-        <ScholarArticleWrapper name="saw nay kaw htoo">
-          <ParagraphWrapper>
-            Hi, I’m Saw Nay Kaw Htoo! After graduating from high school, I did
-            not give too much thought about pursuing a bachelor’s degree – only
-            two people from my village went to university. Instead, I responded
-            to a strong calling I’ve had since I was young, which was to engage
-            in community work.
-          </ParagraphWrapper>
-
-          <ParagraphWrapper>
-            Hence, after high school graduation, I trained and qualified as a
-            Community Health Worker at Mae Tao Clinic at the Thai-Myanmar
-            border, providing medical treatment to villagers and refugees. I am
-            grateful that during my two years there, I was able to care for the
-            ill and weak. However, I also noticed other problems that our
-            patients were mired in, including poverty and threats from armed
-            conflicts. I began to develop an interest in advancing broader
-            social justice.
-          </ParagraphWrapper>
-
-          <ParagraphWrapper>
-            In order to learn more about law, social sciences and political
-            sciences, I completed a diploma at the Peace Law Academy.
-            Afterwards, I joined a United Nations-funded NGO called the
-            “Adventist Development and Relief Agency (ADRA) Myanmar”. As the
-            Community Mobilizer, I liaised with village leaders in executing
-            community-wide projects like a community-based health insurance
-            scheme. Contributing to such projects gave me an unprecedented sense
-            of fulfilment. I wished to follow the footsteps of my colleagues who
-            had bachelor’s degrees, who could play even greater roles such as
-            planning and design. Hence, I completed the General Education
-            Diploma and was very happy to be accepted by Chiang Mai University,
-            where I am currently studying Social Sciences.
-          </ParagraphWrapper>
-
-          <ParagraphWrapper>
-            To me, education is about empowerment – I hope to empower myself and
-            thus to empower others. Myanmar has been through a lot in recent
-            years, so community work will be ever-more important. With my
-            passion, knowledge and experience, I hope to engage in meaningful
-            projects and to contribute to society.
-          </ParagraphWrapper>
-        </ScholarArticleWrapper>
-
-        <ScholarProfile
-          profilePicture={nayKawHtoo}
-          name="Saw Nay Kaw Htoo"
-          ethnicity="Karen"
-          placeOfBirth="Karen State, Myanmar"
-          age="29"
-          programs={programs}
-          goals={goals}
-          facts={facts}
-        />
-      </MainContentWrapper>
-    </div>
-  );
-};
+    </MainContentWrapper>
+  </div>
+);
 
 export default NayKawHtoo;
--- src/pages/scholars/NayMin.jsx
+++ src/pages/scholars/NayMin.jsx
@@ -1,7 +1,107 @@
 import React from "react";
 
+import { ScholarHeader } from "../../components/HeaderImage";
+import MainContentWrapper, {
+  ParagraphWrapper,
+} from "../../components/MainContentWrapper";
+import ScholarArticleWrapper from "../../components/ScholarArticleWrapper";
+import ScholarProfile from "../../components/ScholarProfile";
+
+import naymin from "../../assets/scholars/naymin.png";
+import naymin1 from "../../assets/scholars/naymin/naymin1.jpg";
+import naymin2 from "../../assets/scholars/naymin/naymin2.jpg";
+import naymin3 from "../../assets/scholars/naymin/naymin3.jpg";
+
+const images = [naymin1, naymin2, naymin3];
+
+const programs = [
+  "Rangsit University, Bangkok",
+  "Civil Engineering",
+  "3rd Year in the programme",
+];
+
+const facts = [
+  {
+    question: "If you could have dinner with a famous person, who would it be?",
+    answer:
+      "Jack Ma. He started from zero but attained incredible success through utter determination. It would be a huge privilege if I could meet him!",
+  },
+  {
+    question: "Who is the most inspirational person in your life?",
+    answer:
+      "My Dad. He was a primary school drop-out and really struggles to support my family. Nonetheless, he has always been generous to me (when I was young and didn't know better, I always asked him for pocket money and he would never refuse!). Hard labour has taken a toll on his health. I hope I can be as selfless as him.",
+  },
+];
+
 const NayMin = () => {
-  return <div>NayMin</div>;
+  return (
+    <div>
+      <ScholarHeader
+        imgUrl="bg-[url('/src/assets/programmes.jpg')]"
+        isShading
+        name="nay min"
+        program="Civil Engineering"
+        university="Rangsit University"
+      />
+      <MainContentWrapper>
+        <ScholarArticleWrapper name="nay min">
+          <ParagraphWrapper>
+            My name is Saw Nay Min. I am a Burmese student studying Civil
+            Engineering at Rangsit University, Bangkok, Thailand.
+          </ParagraphWrapper>
+          <ParagraphWrapper>
+            When I was young, I lived in Myanmar with my family. However, my
+            parents couldn’t afford my school fees so I was sent to a migrant
+            school on the Thai-Myanmar border. I used to visit my family once a
+            year. I remember those journeys vividly: many parts of the road,
+            particularly those between Myawaddy to Hpa An, would be out of
+            repair. They would even be inundated during the rainy season. The
+            long and unforgiving commutes seared into my memory. Another
+            memorable aspect was the stark change in landscape as we traveled
+            from Thailand to Myanmar: unlike our more advanced neighbour,
+            Myanmar’s infrastructure was deplorable. The contrast became even
+            sharper as I traveled to my village, which could only be reached by
+            winding muddy paths.
+          </ParagraphWrapper>
+          <ParagraphWrapper>
+            I came to realize that poor infrastructure was as much a cause as a
+            consequence of our economic problems. For example, it took a day to
+            travel from my village to the nearest town, making any high volume
+            trade impossible. That's how I decided to become a civil engineer.
+          </ParagraphWrapper>
+          <ParagraphWrapper>
+            After studying the General Education Diploma in my migrant high
+            school, I enrolled in the civil engineering programme at Rangsit
+            University. University has not been easy. I encountered problems
+            because I was unaccustomed to the teaching method. However, with
+            much determination, I am glad to say I’ve made significant
+            improvements compared to my 1st year and am currently doing quite
+            well.
+          </ParagraphWrapper>
+          <ParagraphWrapper>
+            I still have much to learn about Civil Engineering, but I am ever
+            grateful for the opportunity to be transformed an ordinary boy into
+            an engineer-to-be. I thank Connecting Myanmar for supporting me
+            financially, and also my dearest high school teacher Steve Gomersall
+            for believing in me. I am eager to acquire the knowledge that would
+            help me make a difference to my home country. Please stay tuned as I
+            pave my way to the future.
+          </ParagraphWrapper>
+        </ScholarArticleWrapper>
+
+        <ScholarProfile
+          profilePicture={naymin}
+          name="Saw Nay Min"
+          age="23"
+          ethnicity="Karen"
+          placeOfBirth="Naung Ka Top, Bee Lin, Mon State, Myanmar"
+          programs={programs}
+          facts={facts}
+          images={images}
+        />
+      </MainContentWrapper>
+    </div>
+  );
 };
 
 export default NayMin;
